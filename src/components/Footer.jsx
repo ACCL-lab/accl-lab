1e5237ccb28a71ec26f836cff3cbd85030536cd2
KishantLab <kishant@iitbhilai.ac.in>
2025-08-04 19:00:30 +0530

    contact updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ export default function Footer({ handleOpen }) {
             <p>
               <FaEnvelope className="me-2 text-danger" />
               <span>vishwesh@iitbhilai.ac.in</span><br />
-              <FaPhoneAlt className="me-2 text-danger mt-2" />
+              // <FaPhoneAlt className="me-2 text-danger mt-2" />
               // <span>+91-771-1234567</span>
             </p>
           </div>
